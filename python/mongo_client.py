0251514e61b0a1d0c628cfbb8b48fb5d7ff725f2
login <asimabazi@hotmail.no>
2022-03-08 19:25:29 +0100

    working locally (docker) and in cloud mongodb instances.🎉

--- python/mongo_client.py
+++ python/mongo_client.py
@@ -1,5 +1,6 @@
 import os
-from typing import Collection, Dict
+import time
+from typing import Tuple, Collection, Dict, List
 from dotenv import load_dotenv
 import pymongo
 import requests
@@ -15,7 +16,10 @@ class ConnectionString:
         username = os.getenv("username")
         password = os.getenv("password")
         addr = os.getenv("addr")
-        conn_str = f"mongodb+srv://{username}:{password}@{addr}.mongodb.net/?retryWrites=true&w=majority"
+        cloud = "+srv" if os.getenv("isCloud") else ""
+        cloudNet = ".mongodb.net" if os.getenv("isCloud") else ""
+
+        conn_str = f"mongodb{cloud}://{username}:{password}@{addr}{cloudNet}/?retryWrites=true&w=majority"
 
         return conn_str
 
@@ -28,17 +32,18 @@ class RandomApi:
 
     def __init__(self) -> None:
         self.api_url = "https://randomuser.me/api"
-        pass
+        print(self.api_url)
 
-    def get_random_users(self, amount: int = 1000) -> tuple[bool, Dict]:
+    def get_random_users(self, amount: int = 1000) -> Tuple[bool, Dict]:
         if amount < 1 or amount > 5000:
             print("It is only possible to retreive 1-5000 random users.")
 
         url = f"{self.api_url}/?results={str(amount)}"
-        print(f"url: {url}")
-        r = requests.get(url)
+        print(f"url: {url}, doing request")
+        r = requests.get(url, timeout=5)
 
         if r.status_code == 200:
+            print("success")
             return (True, r.json())
         else:
             print(r.status_code)
@@ -52,14 +57,14 @@ class MongoHub:
         self.client = pymongo.MongoClient(
             self.cs.get(), serverSelectionTimeoutMS=5000)
 
-        (is_connected, db_names) = self.is_connection_establised()
+        (is_connected, db_names) = self.is_connection_established()
         if (is_connected):
             print(f"Successfully connected, available databases: {db_names}")
 
         if database:
             self.db = self.client[database]
 
-    def is_connection_establised(self) -> tuple[bool, list[str]]:
+    def is_connection_established(self) -> Tuple[bool, List[str]]:
 
         try:
             names = self.client.list_database_names()
@@ -76,7 +81,7 @@ class MongoHub:
         return False
 
     def set_db(self, database: str) -> bool:
-        (is_connected, dbs) = self.is_connection_establised
+        (is_connected, dbs) = self.is_connection_established()
 
         if is_connected and database:
             self.db = self.client[database]
@@ -93,17 +98,18 @@ class MongoHub:
         if collection_name in collection_list and should_purge_old:
             col.delete_many({})
 
+        start_time = time.time()
         col.insert_many(values)
+        print(f"Took: {time.time() - start_time} seconds to insert {len(values)} elements.")
 
         return col
 
 
 mh = MongoHub("py")
+amountOfUsers = 5000
 random_users_api = RandomApi()
-success, res = random_users_api.get_random_users(100)
-
+success, res = random_users_api.get_random_users(amountOfUsers)
 
 if success:
     users = res["results"]
-
     mh.create_collection("users", users, True)
